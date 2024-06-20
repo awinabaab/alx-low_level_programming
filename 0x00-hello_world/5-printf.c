6f404be9a210f1310e1637b2293001bc9d1e71dd
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-06-20 10:16:52 +0000

    Add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a specific phrase
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	char phrase[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", phrase);
+	return (0);
+}
