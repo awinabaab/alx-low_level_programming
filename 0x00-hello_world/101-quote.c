f9876d788dd7af3a6d0ef56ab71358abf77cd4a8
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-06-20 11:07:15 +0000

    Add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Prints a phrase string without using prinf() of puts()
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2013-10-19\n";
+
+	write(2, str, 59);
+	return (1);
+}
