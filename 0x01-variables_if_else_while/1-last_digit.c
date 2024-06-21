37728897bbda754f6f121f63de7f94c7fe1d6d90
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-06-21 12:23:02 +0000

    Remove extra whitespace from output formatted string

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,6 +25,6 @@ int main(void)
 	else if (last_num > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_num);
 	else
-		printf("Last digit of %d is %d and is  0\n", n, last_num);
+		printf("Last digit of %d is %d and is 0\n", n, last_num);
 	return (0);
 }
