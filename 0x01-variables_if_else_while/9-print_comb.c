3d7ecd51d1ddb0c39e7b6cf7662ea2f1bc0b3290
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-06-21 13:58:55 +0000

    Add 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Prints all possible combinations of single-digit numbers
+ * separated by a comma (,), followed by a space.
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+	}
+	return (0);
+}
