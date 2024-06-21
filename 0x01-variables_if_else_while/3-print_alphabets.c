3580c8620e4142ded542881ca67999824e316b98
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-06-21 12:28:28 +0000

    Remove for loop initial declaration

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,11 @@
 
 int main(void)
 {
-	for (char alpha = 'a'; alpha <= 'z'; alpha++)
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 		putchar(alpha);
-	for (char alpha = 'A'; alpha <= 'Z'; alpha++)
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
 		putchar(alpha);
 	putchar('\n');
 	return (0);
