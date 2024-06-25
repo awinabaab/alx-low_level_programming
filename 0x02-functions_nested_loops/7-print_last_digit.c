af1942b2067d2285918443e968faa9c5f5d0ba1f
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-06-25 13:55:02 +0000

    Add condition for negative arguments

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,15 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n < 0)
-		n = -n;
-
 	last_digit = n % 10;
-	_putchar(last_digit + 48);
+
+	if (last_digit < 0)
+	{
+		last_digit = -last_digit;
+		_putchar(last_digit + 48);
+	}
+	else
+		_putchar(last_digit + 48);
 
 	return (last_digit);
 }
