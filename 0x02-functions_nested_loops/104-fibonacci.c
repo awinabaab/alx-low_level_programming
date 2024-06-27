3ee08a02785ed09402247a8b610a083783426f60
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-06-27 06:27:08 +0000

    Hard code fibonacci numbers 1 and 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,27 +2,39 @@
 
 /**
  * main - finds and prints the first 98 fibonacci numbers starting with 1 and 2
- *
  * Return: 0
  */
 
 int main(void)
 {
 	int count;
-	unsigned long fibonacci0, fibonacci1, temp;
+	unsigned long fibonacci1, fibonacci2, temp;
+	unsigned long fibonacci01, fibonacci02, fibonacci11, fibonacci12;
 
-	fibonacci0 = 0;
 	fibonacci1 = 1;
-
-	for (count = 2; count < 99; count++)
+	fibonacci2 = 2;
+	printf("%lu, %lu", fibonacci1, fibonacci2);
+	for (count = 3; count < 92; count++)
 	{
-		temp = fibonacci1;
-		fibonacci1 += fibonacci0;
-		fibonacci0 = temp;
-		if (count < 99)
-			printf("%lu, ", fibonacci1);
+		temp = fibonacci2;
+		fibonacci2 += fibonacci1;
+		fibonacci1 = temp;
+		if (count < 92)
+			printf(", %lu", fibonacci2);
 	}
-	printf("%lu\n", fibonacci1);
-
+	fibonacci01 = fibonacci1 / 1000000000;
+	fibonacci02 = fibonacci1 % 1000000000;
+	fibonacci11 = fibonacci2 / 1000000000;
+	fibonacci12 = fibonacci2 % 1000000000;
+	for (count = 92; count < 99; count++)
+	{
+		fibonacci11 = fibonacci11 + fibonacci01;
+		fibonacci01 = fibonacci11 - fibonacci01;
+		fibonacci12 = fibonacci12 + fibonacci02;
+		fibonacci02 = fibonacci12 - fibonacci02;
+		printf(", %lu", fibonacci11 + (fibonacci12 / 1000000000));
+		printf("%lu", fibonacci12 % 1000000000);
+	}
+	printf("\n");
 	return (0);
 }
