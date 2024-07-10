639b69e9ecb8a78ca49ee8a9db6fb4b6b26a4360
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-07-10 10:44:11 +0000

    Add 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string
+ * @s: String length to be found
+ *
+ * Return: Length of @s
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (_strlen_recursion(s + 1) + 1);
+}
