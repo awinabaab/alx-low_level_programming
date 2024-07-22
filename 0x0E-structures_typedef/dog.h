226ccf2d34abb1e4e235a53e75c47a1885204fae
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-07-22 07:27:38 +0000

    Add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog structure
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Name of Dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif /* ifndef DOG_H */
