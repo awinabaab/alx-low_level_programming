e62c4cb14157fcdaeeb3513e144adf5a4f030d95
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-07-22 09:56:52 +0000

    Check for NULL pointer

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees dog_t
+ * @d: dog to be freed
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if(d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
