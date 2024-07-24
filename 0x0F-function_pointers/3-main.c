20d86a3848e47a5f9b6ef605f3eb0c6bafe5cb91
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-07-24 10:47:57 +0000

    Add task #3 files

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,46 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argv: Array of command line arguments
+ * @argc: Size of @argv
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int (*op_func)(int, int);
+	int num1, num2, op_result;
+	char op = *argv[2];
+
+	if (argc > 4 || argc < 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	op_func = get_op_func(argv[2]);
+
+	if (!op_func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((op == '/' || op == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	op_result = op_func(num1, num2);
+
+	printf("%d\n", op_result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,70 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Returns the sum of two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: @a + @b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - Returns the difference of two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: @a - @b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - Returns the product of two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: @a * @b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - Returns the division of two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Sum of @a + @b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - Returns the remainder of the division two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Sum of @a + @b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
