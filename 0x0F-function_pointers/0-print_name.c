551283f2c930807832d49908f3d5484528243b73
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-07-24 09:04:24 +0000

    Betty style standard and check for NULL pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,28 +3,14 @@
 /**
  * print_name - Prints a name
  * @name: Name to be printed
- * (*f) - Function pointer
- * @n: String to be printed
+ * @f: Function pointer
  *
  * Return: void
  */
 
-void print_name(char *name, void (*f)(char *n))
+void print_name(char *name, void (*f)(char *))
 {
+	if (!name || !f)
+		return;
 	f(name);
-}
-
-/**
- * f - Prints a string passed to it
- * @n: String to be printed
- *
- * Return: void
- */
-
-void f(char *n)
-{
-	int sub;
-
-	for (sub = 0; n[sub] != '\0'; sub++)
-		_putchar(n[sub]);
 }
