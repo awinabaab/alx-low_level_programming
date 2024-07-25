8f49dbd3d88c81aaa30c3b2fe9dbd3b578d8be89
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-07-25 05:09:01 +0000

    Refactor

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,9 +14,8 @@ int main(int argc, char **argv)
 {
 	int (*op_func)(int, int);
 	int num1, num2, op_result;
-	char op = *argv[2];
 
-	if (argc > 4 || argc < 3)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
@@ -33,7 +32,7 @@ int main(int argc, char **argv)
 		exit(99);
 	}
 
-	if ((op == '/' || op == '%') && num2 == 0)
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
