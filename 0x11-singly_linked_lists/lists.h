022ecc44ed80e20143539e53d2310e5e0e1116a7
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-08-01 10:53:26 +0000

    Add add_node function prototype

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -25,4 +25,7 @@ size_t print_list(const list_t *h);
 /* Returns the number of elements in a linked list_t list */
 size_t list_len(const list_t *h);
 
+/* Adds a new node at the beginning of a list_t list */
+list_t *add_node(list_t **head, const char *str);
+
 #endif /* ifndef LISTS_H */
