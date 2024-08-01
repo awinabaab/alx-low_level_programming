f6b33320ac20c909d94a33453b4081891d437d89
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-08-01 17:05:57 +0000

    Add 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * first - Prints a string before the main function is executed
+ *
+ * Return: void
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
