ef37cc1e6554519aca7293fc147c8277d1aa678f
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-08-01 09:55:39 +0000

    Add lists.h

---
+++ 0x11-singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct list_s - Singly linked list
+ * @str: String - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: Singly linked list node structure
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/* Prints all elements of a list_t list */
+size_t print_list(const list_t *h);
+
+#endif /* ifndef LISTS_H */
