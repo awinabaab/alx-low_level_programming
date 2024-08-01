3014cd47979ed3fb395f9beb59ae42c1f514b2dd
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-08-01 10:00:02 +0000

    Add 0-print_list.c

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - Prints all the elements of a list_t list
+ * @h: List to be printed
+ *
+ * Return: The number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count;
+
+	for (count = 0; h; count++, h = h->next)
+		printf("[%d] %s\n", h->str ? h->len : 0, h->str ? h->str : "(nil)");
+
+	return (count);
+}
