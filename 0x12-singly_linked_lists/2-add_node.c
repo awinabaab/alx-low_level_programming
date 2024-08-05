95742405236c8f172aea9bcafb37cdee37c491c8
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-08-05 15:16:21 +0000

    Handle NULL head case

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -22,7 +22,11 @@ list_t *add_node(list_t **head, const char *str)
 
 	new->str = strdup(str);
 	new->len = strlen(str);
-	new->next = *head;
+
+	if (!*head)
+		new->next = NULL;
+	else
+		new->next = *head;
 	*head = new;
 
 	return (new);
