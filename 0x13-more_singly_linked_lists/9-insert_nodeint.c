07ebfaad91a336291eaee25926685760d3c49d11
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-08-06 17:59:01 +0000

    Update readme

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,7 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	node = *head;
-	for (pos = 0; pos < idx - 1; pos++)
+	for (pos = 0; pos < idx; pos++)
 		node = node->next;
 
 	if (!node)
