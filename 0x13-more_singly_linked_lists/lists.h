5be8362b624e19072016a0faab62759b7aae9adf
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-08-06 09:52:57 +0000

    Add sum_listint function prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,12 +17,31 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* Prints all the elements of a listint_t list */
 size_t print_listint(const listint_t *h);
+
+/* Returns the number of elements in a linked listint_t list */
 size_t listint_len(const listint_t *h);
+
+/* Adds a node at the beginning of a listint_t linked list */
 listint_t *add_nodeint(listint_t **head, const int n);
+
+/* Adds a node at the end of a listint_t list */
 listint_t *add_nodeint_end(listint_t **head, const int n);
+
+/* Frees a listint_t list */
 void free_listint(listint_t *head);
+
+/* Frees a listint_t list and sets @head to NULL */
 void free_listint2(listint_t **head);
+
+/* Deletes the head node of a listint_t linked list */
 int pop_listint(listint_t **head);
+
+/* Returns the nth node of a listint_t linked list */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+
+/* Sums all the data(n) of a listint_t linked list */
+int sum_listint(listint_t *head);
+
 #endif /* #ifndef LISTS_H */
