5508fdaf0511459b71bad93bb78fb4d7c5f44e5e
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-08-08 14:00:24 +0000

    Add set_bit function prototype

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,4 +7,7 @@ unsigned int binary_to_uint(const char *b);
 /* Returns the value of a bit at a given index */
 int get_bit(unsigned long int n, unsigned int index);
 
+/* Sets the value of a bit to 1 at a given index */
+int set_bit(unsigned long int *n, unsigned int index);
+
 #endif /* ifndef MAIN_H */
