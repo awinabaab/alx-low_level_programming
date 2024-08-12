caa7a7fc2ef4378aa4d7b76289c0ce1bfa58ba86
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-08-12 13:06:40 +0000

    Add append_text_to_file function prototype

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,4 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 /* Creates a file */
 int create_file(const char *filename, char *text_content);
 
+/* Appends a text at the end of a file */
+int append_text_to_file(const char *filename, char *text_content);
+
 #endif /* ifndef MAIN_H */
