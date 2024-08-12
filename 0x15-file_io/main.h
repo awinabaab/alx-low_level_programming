9b2ea7943ce027b9b5c81c2cbac47e22e6811639
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-08-12 11:58:04 +0000

    Add read_textfile function prototype

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,8 +3,15 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 /* Writes a character to stdout */
 int _putchar(char c);
+
+/* Reads a text file and prints it to the POSIX standard output */
+ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif /* ifndef MAIN_H */
