29266675f346821a2b4c5b4f491c01b815101825
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-08-12 15:13:52 +0000

    Add close_function prototype and define buffer size

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define BUF_SIZE 1024
+
 /* Writes a character to stdout */
 int _putchar(char c);
 
@@ -19,5 +21,8 @@ int create_file(const char *filename, char *text_content);
 
 /* Appends a text at the end of a file */
 int append_text_to_file(const char *filename, char *text_content);
+
+/* Closes an open file */
+void close_file(int);
 
 #endif /* ifndef MAIN_H */
