493785d6938edcff2fd511f2d8c064ce1e6ed97d
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-08-13 05:53:04 +0000

    Define error messages

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,10 @@
 #include <fcntl.h>
 
 #define BUF_SIZE 1024
+#define USAGE_ERR "Usage: cp file_from file_to\n"
+#define READ_ERR  "Error: Can't read from file %s\n"
+#define WRITE_ERR "Error: Can't write to %s\n"
+#define CLOSE_ERR "Error: Can't close fd %d\n"
 
 /* Writes a character to stdout */
 int _putchar(char c);
