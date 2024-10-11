992a6118e18b9082d3ca7ee59332cb7c08f17cb5
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-10-11 07:01:58 +0000

    Add 3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table
+ * @ht: the hash table
+ * @key: key
+ * @value: value associated with @key
+ *
+ * Return: 1 if succeeded, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int hash = 0;
+	unsigned long int index = 0;
+	hash_node_t *new_node = NULL;
+
+	if (!key)
+		return (0);
+
+	hash = hash_djb2((unsigned char *)key);
+	index = hash % ht->size;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+
+	new_node->key = (char *)key;
+	new_node->value = strdup(value);
+
+	if (ht->array[index])
+	{
+		new_node->next = ht->array[index];
+		ht->array[index] = new_node;
+	}
+	else
+		ht->array[index] = new_node;
+
+	return (1);
+}
