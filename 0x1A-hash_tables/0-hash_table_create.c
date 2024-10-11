fe8a013ea2df6268b468c668b6d11f73f6b16610
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-10-11 08:02:47 +0000

    Assign array of hash nodes to NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,14 +11,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = NULL;
+	unsigned long int i = 0;
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (!hash_table)
 		return (NULL);
 
-	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	hash_table->array = malloc(sizeof(hash_node_t) * size);
 	if (!hash_table->array)
 		return (NULL);
+
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
 
 	hash_table->size = size;
 
