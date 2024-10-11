32b39b0820fc3d24437e9185e912b40e3d5958f1
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-10-11 08:03:34 +0000

    Initialize hash)node to NULL

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,12 +14,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int hash = 0;
 	unsigned long int index = 0;
-	hash_node_t *new_node;
+	hash_node_t *new_node = NULL;
 
 	if (!ht)
 		return (0);
 
-	if (key == "")
+	if (!key)
 		return (0);
 
 	hash = hash_djb2((const unsigned char *)key);
