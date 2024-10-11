eb4d3303f13b74ed3c327ba856d40e53f9724c6f
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-10-11 16:01:22 +0000

    Add 5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints hash table
+ * @ht: the hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node = NULL;
+	unsigned long int size = 0;
+	unsigned long int idx = 0;
+	int comma = 1;
+
+	if (!ht)
+		return;
+
+	size = ht->size;
+
+	printf("{");
+	for (idx = 0; idx < size; idx++)
+	{
+		node = ht->array[idx];
+		while (node)
+		{
+			if (!comma)
+				printf(", ");
+			printf("'%s' : '%s'", node->key, node->value);
+			comma = 0;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
