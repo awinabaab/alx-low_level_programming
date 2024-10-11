7a704c7c05ab6f8e1b4de6c9c68f3d0e9a408c18
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-10-11 08:10:57 +0000

    Check for empty key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht)
 		return (0);
 
-	if (!key)
+	if (*key == '\0')
 		return (0);
 
 	hash = hash_djb2((const unsigned char *)key);
