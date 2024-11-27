81beec847d684080e4e5219df68768220eeee184
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-11-27 18:31:37 +0000

    Set boundaries

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """Island Perimeter module"""
 
+
 def island_perimeter(grid):
-    """Returns the area of the island described in grid"""
+    """Returns the perimeter of the island described in grid"""
     perimeter = 0
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if i <= 0 or not grid[i - 1][j]:
+                if i <= 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                if i >= len(grid) - 1 or not grid[i + 1][j]:
+                if i >= len(grid) - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
-                if j <= 0 or not grid[i][j - 1]:
+                if j <= 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-                if j >= len(grid) - 1 or not grid[i][j + 1]:
+                if j >= len(grid[i]) - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
-    
+
     return (perimeter)
